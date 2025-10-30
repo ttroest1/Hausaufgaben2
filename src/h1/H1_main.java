3d2e1aad2189825cceff3d9afd50c0edaecb2f19
ttroest1 <ttroest1@smail.uni-koeln.de>
2025-10-30 18:01:16 +0100

    h1 bearbeitet mit println Ausgabe von z

---
+++ src/h1/H1_main.java
@@ -0,0 +1,15 @@
+package h1;
+
+public class H1_main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int z = 2;
+		if(z>10) {
+			z-=10;
+		}
+		System.out.println("z hat den Wert "+z);
+	}
+
+}
