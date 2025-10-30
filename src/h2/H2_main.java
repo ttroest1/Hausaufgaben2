5f9123610a20799a13366c93047377ea807e2e80
ttroest1 <ttroest1@smail.uni-koeln.de>
2025-10-30 18:20:11 +0100

    h2 bearbeitet mit Println Ausgabe für min und max

---
+++ src/h2/H2_main.java
@@ -0,0 +1,31 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 14;
+		int j = 1;
+		int k = -2;
+		int min = -400;
+		int max = -90;
+		
+		min = i;
+		if(min>j) {
+			min = j;
+			if(min>k) {
+				min = k;	
+			}
+		}
+		max = i;
+		if(max<j) {
+			max = j;
+			if(max<k) {
+				max = k;
+			}
+		}
+		System.out.println("Minimum ist: "+min);
+		System.out.println("Maximum ist: "+max);		
+	}
+}
+	
