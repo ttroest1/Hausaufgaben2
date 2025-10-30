fe42fd8112ba92a96aad8a68abaa9004e41c7fe3
ttroest1 <ttroest1@smail.uni-koeln.de>
2025-10-30 17:37:14 +0100

    P1 bearbeitet

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,5 +1,20 @@
 package p1;
 
-public class P1_main {
+import java.util.Scanner;
 
+public class P1_main {
+	
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+	
+		String zeile = new String();
+	
+		zeile = s.nextLine();
+	
+		System.out.println(zeile);
+	
+		s.close();
+	
+	}
 }
